65fed01c7c0552037f71970a353c8a92cf2d963c
arslan jaffar <arslanbinjaffar@gmail.com>
2025-01-18 18:25:49 +0500

    Update environment configuration for GitHub Pages deployment and adjust asset paths in HTML

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-const IS_GITHUB_PAGE_DEPLOY = process.env.VITE_IS_GITHUB_PAGE_DEPLOY === "true";
-const base = IS_GITHUB_PAGE_DEPLOY ? "/temp-mail-react-js" : "/";
-console.log(base, "base");
-export default defineConfig({
-  plugins: [react()],
-  base,
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const IS_GITHUB_PAGE_DEPLOY = env.VITE_IS_GITHUB_PAGE_DEPLOY === "true";
+  const base = IS_GITHUB_PAGE_DEPLOY ? "/temp-mail-react-js/" : "/";
+  console.log(base, "base");
+
+  return {
+    plugins: [react()],
+    base,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+  };
 });
